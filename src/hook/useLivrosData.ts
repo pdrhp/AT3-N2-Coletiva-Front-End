fd919f3b0421f1e49df0ba2eee55f4b96b4f75bd
Pedro César <pedroocesarmf@gmail.com>
2024-06-15 17:49:35 -0300

    fix: fixed the hook that fetch data

--- src/hook/useLivrosData.ts
+++ src/hook/useLivrosData.ts
@@ -6,7 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 function useLivrosData(titulo?: string) {
     const query = useQuery({
         queryFn: () => getLivros(titulo),
-        queryKey: ['livros-data']
+        queryKey: ['livros-data'],
+        select: (data) => data.data
     })
 
     return query;
