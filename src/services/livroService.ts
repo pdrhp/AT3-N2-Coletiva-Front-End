82926ad7321970e6b1c5c491803e1d7767f8e451
Pedro César <pedroocesarmf@gmail.com>
2024-06-15 17:06:56 -0300

    fix: fixed the url query

--- src/services/livroService.ts
+++ src/services/livroService.ts
@@ -3,7 +3,7 @@ import client from "./client"
 
 
 export const getLivros = async (titulo?: string) => {
-    const response = await client.get(`livros?titulo=${titulo}`);
+    const response = await client.get(`livros?titulo=${titulo ? titulo : ''}`);
     console.log(response);
     return response.data; 
 }
